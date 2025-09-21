18410e6b305662f4306ee2d2e64449555b0f12d5
Arav-Mathur <aravmathur007@gmail.com>
2025-09-21 16:53:10 -0400

    did costmap. hopefully it works?

--- src/robot/costmap/include/costmap_node.hpp
+++ src/robot/costmap/include/costmap_node.hpp
@@ -3,20 +3,28 @@
  
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
- 
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
 #include "costmap_core.hpp"
  
 class CostmapNode : public rclcpp::Node {
   public:
     CostmapNode();
-    
+    void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg);
     // Place callback function here
-    void publishMessage();
+    void publishCostMap();
  
   private:
     robot::CostmapCore costmap_;
     // Place these constructs here
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr string_pub_;
+    int width = 100;
+    int height = 100;
+    float resolution = 0.1f;
+    float inflation_radius = 1.0f;
+    std::vector<std::vector<int>> OccupancyMap;
+
+    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_pub_;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar_sub_;
     rclcpp::TimerBase::SharedPtr timer_;
 };
  
--- src/robot/costmap/src/costmap_node.cpp
+++ src/robot/costmap/src/costmap_node.cpp
@@ -1,20 +1,82 @@
 #include <chrono>
 #include <memory>
- 
+#include <vector>
+#include <cmath>
+
 #include "costmap_node.hpp"
- 
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
+
+
 CostmapNode::CostmapNode() : Node("costmap"), costmap_(robot::CostmapCore(this->get_logger())) {
+
+  OccupancyMap.resize(height, std::vector<int>(width, 0));
   // Initialize the constructs and their parameters
-  string_pub_ = this->create_publisher<std_msgs::msg::String>("/test_topic", 10);
-  timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&CostmapNode::publishMessage, this));
+  costmap_pub_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>("/costmap", 10);
+  lidar_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>("/lidar", 10, std::bind(&CostmapNode::laserCallback, this, std::placeholders::_1));
+
+  timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&CostmapNode::publishCostMap, this));
 }
- 
+void CostmapNode::laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan) {
+  for (auto &row : OccupancyMap) std::fill(row.begin(), row.end(), 0.0);
+
+  for (size_t i = 0; i < scan->ranges.size(); i++)
+  {
+    float angle = scan->angle_min + (i * scan->angle_increment);
+    float range = scan->ranges[i];
+    if (range < scan->range_max && range > scan->range_min)
+    {
+      int x_grid =  static_cast<int>((range * cos(angle))/resolution);
+      int y_grid =  static_cast<int>((range * sin(angle))/resolution);
+      OccupancyMap[y_grid][x_grid] = 100;
+     }
+    
+  }
+  
+  int inflation_cells = static_cast<int>(inflation_radius / resolution);
+  for (size_t y = 0; y < height; y++){
+    for (size_t x = 0; x < width; x++){
+      if(OccupancyMap[y][x] == 100){
+          for (size_t sy = -inflation_cells; sy <= inflation_cells; sy++){
+            for (size_t sx = -inflation_cells; sx <= inflation_cells; sx++){
+              if ( x+sx >= 0 && x+sx < width && y+sy >= 0 && y+sy < height ){ //check in map
+                float e_distance = sqrt((sx*sx + sy*sy)) * resolution;
+                if(e_distance<inflation_radius){
+                  float cost = 100*(1-(e_distance/inflation_radius));
+                  if (cost> OccupancyMap[y+sy][x+sx]){
+                    OccupancyMap[y+sy][x+sx] = static_cast<int>(cost);
+                  }
+                }
+              }
+            }
+          }
+      }
+    }
+    
+  }
+}
 // Define the timer to publish a message every 500ms
-void CostmapNode::publishMessage() {
-  auto message = std_msgs::msg::String();
-  message.data = "Hello, ROS 2!";
-  RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-  string_pub_->publish(message);
+void CostmapNode::publishCostMap() {
+  nav_msgs::msg::OccupancyGrid msg;
+
+  msg.header.stamp = this->now();
+  msg.header.frame_id = "map";
+  msg.info.resolution = 0.1;
+  msg.info.width = width;
+  msg.info.height = height;
+
+  msg.info.origin.position.x = 0.0;
+  msg.info.origin.position.y = 0.0;
+  msg.info.origin.position.z = 0.0;
+  msg.info.origin.orientation.w = 1.0;
+
+  msg.data.resize(width * height);
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            msg.data[y * width + x] = static_cast<int8_t>(OccupancyMap[y][x]);
+        }
+    }
+  costmap_pub_->publish(msg);
 }
  
 int main(int argc, char ** argv)
